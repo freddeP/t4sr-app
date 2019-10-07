2ad063b1ae603090a839adf7f1f76b5cc01ccc02
Fredric Persson <freper7912@skynet.halmstad.se>
2019-10-07 15:47:37 +0200

    lagt till program

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 async function getAllChannels()
 {
     const url = "http://api.sr.se/api/v2/channels?format=json&pagination=false";
@@ -17,9 +16,37 @@ function printChannels(channels){
     let html = channels.map(channel=>{
 
         return `
-            <div>
+            <div onclick = loadPrograms(${channel.id})>
                 <h5>${channel.name}</h5>
                 <img src = "${channel.image}">
+            </div>
+        `;
+
+    });
+
+    document.getElementsByClassName("content")[0].innerHTML=html.join("");
+
+
+}
+
+
+async function loadPrograms(id)
+{
+    const url = `http://api.sr.se/api/v2/programs/index?channelid=${id}&format=json&pagination=false`;
+    const data = await fetch(url);
+    const programs = (await data.json()).programs;
+    console.table(programs);
+    printPrograms(programs);
+}
+
+function printPrograms(programs){
+
+    let html = programs.map(program=>{
+
+        return `
+            <div>
+                <h5>${program.name}</h5>
+                <p>${program.description}</p>
             </div>
         `;
 
